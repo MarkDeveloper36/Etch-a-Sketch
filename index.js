227cf4a8922f8373ecd84a2021e8f5c021fb84cf
Mark <MarkKranenburg@hotmail.nl>
2023-08-04 19:47:32 +0200

    add basic html and css and add containers

---
+++ index.js
@@ -0,0 +1,5 @@
+const grid = document.querySelector('#gridContainer');
+const square = document.createElement('div');
+square.setAttribute('style', 'height: 10px; width: 10px; background: white;');
+
+grid.appendChild(square);
