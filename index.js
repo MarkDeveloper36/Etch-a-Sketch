e4d8cdfd12d9ee2db2f403dcb197e776d4624d00
Mark <MarkKranenburg@hotmail.nl>
2023-08-05 13:45:14 +0200

    add loop for printing squares

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const grid = document.querySelector('#gridContainer');
-const square = document.createElement('div');
-square.setAttribute('style', 'height: 10px; width: 10px; background: white;');
 
-grid.appendChild(square);
+
+
+
+
+let size = 4;
+printSquares(size)
+
+function printSquares(size){
+    for (let i = 0; i < size; i++) {
+        let square = document.createElement('div');
+        square.setAttribute('style', 'height: 10px; width: 10px; background: white; border-bottom: 1px solid gray');
+        grid.appendChild(square);
+        console.log('kj');
+    }
+}
