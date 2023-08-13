d90e41bb8f9722e6fdd3422a7655793d44194d50
Mark <MarkKranenburg@hotmail.nl>
2023-08-13 19:09:26 +0200

    add grid size js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+// grid display
 const grid = document.querySelector('#gridContainer');
 
+// grid controler
+const value = document.querySelector("#value");
+const input = document.querySelector("#pi_input");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+  value.textContent = event.target.value;
+});
 
 
 
-
-let size = 4;
-printSquares(size)
+let size = 2;
+printSquares(size);
 
 function printSquares(size){
     for (let i = 0; i < size; i++) {
         let square = document.createElement('div');
         square.setAttribute('style', 'height: 10px; width: 10px; background: white; border-bottom: 1px solid gray');
         grid.appendChild(square);
-        console.log('kj');
-    }
-}
+    };
+};
