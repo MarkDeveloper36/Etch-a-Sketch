f0f90f44e3bf6163087f3544b26916f72c4a4400
Mark <MarkKranenburg@hotmail.nl>
2023-08-20 17:05:09 +0200

    onload function to print 16 squares

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const grid = document.querySelector('#gridContainer');
 // grid controler
 const displaySize = document.querySelector("#displaySize");
 const inputSize = document.querySelector("#inputSize");
+
+
 displaySize.textContent = inputSize.value;
+
 inputSize.addEventListener("input", () => {
   displaySize.textContent = inputSize.value;
   updateSquares(inputSize.value);
 });
+
+window.onload = function() {
+    for (let i = 0; i < 16; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+    };
+};
 
 
 function updateSquares(size){
