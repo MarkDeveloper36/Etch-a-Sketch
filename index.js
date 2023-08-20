b47ba1e9dcab3f3d290b080d02d94134d53249c3
Mark <MarkKranenburg@hotmail.nl>
2023-08-20 17:35:56 +0200

    make input grid size add and remove sqares

--- index.js
+++ index.js
@@ -2,17 +2,24 @@
 const grid = document.querySelector('#gridContainer');
 
 // grid controler
-const displaySize = document.querySelector("#displaySize");
-const inputSize = document.querySelector("#inputSize");
+const displaySize = document.querySelector('#displaySize');
+const inputSize = document.querySelector('#inputSize');
 
+let chosenSize = 16;
+let squareCount = 16;
 
-displaySize.textContent = inputSize.value;
+//set size display to default value
+displaySize.textContent = chosenSize;
 
-inputSize.addEventListener("input", () => {
-  displaySize.textContent = inputSize.value;
-  updateSquares(inputSize.value);
+//event listener to get input for gridsize
+inputSize.addEventListener('input', () => {
+  chosenSize = inputSize.value;
+  displaySize.textContent = chosenSize;
+  addOrRemoveSquares(squareCount, chosenSize);
 });
 
+
+// print default 16 squares
 window.onload = function() {
     for (let i = 0; i < 16; i++) {
         let square = document.createElement('div');
@@ -22,11 +29,31 @@ window.onload = function() {
 };
 
 
-function updateSquares(size){
-    for (let i = 0; i < size; i++) {
+
+//make desision to add or remove squares
+function addOrRemoveSquares(squareCount, chosenSize) {
+  if (chosenSize > squareCount){
+    addSquares(chosenSize);
+  } else if (chosenSize < squareCount){
+    removeSquares(chosenSize);
+  }
+};
+
+// add squares
+function addSquares(size) {
+    for (let i = squareCount; i < size; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
         grid.appendChild(square);
+        squareCount++;
     };
 };
 
+// removesquares
+function removeSquares(size) {
+  for (let i = squareCount; i > size; i--) {
+    let square = document.querySelector('.square');
+    square.remove();
+    squareCount--;
+  }
+}
