c6105c2299a1c79d29307a389a5f2e285981dba0
Mark <MarkKranenburg@hotmail.nl>
2023-09-19 18:28:22 +0200

    change approach project

--- index.js
+++ index.js
@@ -5,55 +5,28 @@ const grid = document.querySelector('#gridContainer');
 const displaySize = document.querySelector('#displaySize');
 const inputSize = document.querySelector('#inputSize');
 
-let chosenSize = 16;
-let squareCount = 16;
+let newSize = 4;
 
-//set size display to default value
-displaySize.textContent = `${chosenSize} \u2715 ${chosenSize}`;
+//print default 16 squares
+window.onload = function() {
+  for (let i = 0; i < 16; i++) {
+      let square = document.createElement('div');
+      square.classList.add('square', 'squareG1');
+      grid.appendChild(square);
+  };
+};
 
-//event listener to get input for gridsize
-inputSize.addEventListener('input', () => {
-  chosenSize = inputSize.value;
-  displaySize.textContent = `${chosenSize} \u2715 ${chosenSize}`;
-  addOrRemoveSquares(squareCount, chosenSize);
+displaySize.textContent = `${newSize} \u2715 ${newSize}`;
+
+inputSize.addEventListener('change', () => {
+  removeGrid();
+  newSize = inputSize.value;
+  displaySize.textContent = `${newSize} \u2715 ${newSize}`;
 });
 
-
-// print default 16 squares
-window.onload = function() {
-    for (let i = 0; i < 16; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
-    };
-};
-
-
-
-//make desision to add or remove squares
-function addOrRemoveSquares(squareCount, chosenSize) {
-  if (chosenSize > squareCount){
-    addSquares(chosenSize);
-  } else if (chosenSize < squareCount){
-    removeSquares(chosenSize);
-  }
-};
-
-// add squares
-function addSquares(size) {
-    for (let i = squareCount; i < size; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
-        squareCount++;
-    };
-};
-
-// removesquares
-function removeSquares(size) {
-  for (let i = squareCount; i > size; i--) {
-    let square = document.querySelector('.square');
-    square.remove();
-    squareCount--;
-  }
+function removeGrid() {
+  numberOfLoops = grid.childElementCount;
+  for (let i = 0; i < numberOfLoops; i++) {
+    grid.removeChild(grid.lastChild);
 }
+  }
