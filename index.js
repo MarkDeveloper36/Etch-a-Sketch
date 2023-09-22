bdd740956712237c343d7ddbac97b79d442d18a0
Mark <MarkKranenburg@hotmail.nl>
2023-09-22 18:46:05 +0200

    fix adding squares to grid and give them right size

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const displaySize = document.querySelector('#displaySize');
 const inputSize = document.querySelector('#inputSize');
 
 let newSize = 4;
-
 //print default 16 squares
 window.onload = function() {
   for (let i = 0; i < 16; i++) {
@@ -21,6 +20,7 @@ displaySize.textContent = `${newSize} \u2715 ${newSize}`;
 inputSize.addEventListener('change', () => {
   removeGrid();
   newSize = inputSize.value;
+  addGrid()
   displaySize.textContent = `${newSize} \u2715 ${newSize}`;
 });
 
@@ -28,5 +28,33 @@ function removeGrid() {
   numberOfLoops = grid.childElementCount;
   for (let i = 0; i < numberOfLoops; i++) {
     grid.removeChild(grid.lastChild);
-}
   }
+};
+
+function addGrid() {
+  numberOfLoops = (newSize * newSize);
+  for (let i = 0; i < numberOfLoops; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    grid.appendChild(square);
+    giveSquareRightSize(square);
+  }
+};
+
+function giveSquareRightSize(square) {
+  if (newSize == 4) {
+    square.classList.add('squareG4');
+  } else if (newSize == 5) {
+    square.classList.add('squareG5');
+  } else if (newSize == 6) {
+    square.classList.add('squareG6');
+  } else if (newSize == 7) {
+    square.classList.add('squareG7');
+  } else if (newSize == 8) {
+    square.classList.add('squareG8');
+  } else if (newSize == 9) {
+    square.classList.add('squareG9');
+  } else if (newSize == 10) {
+    square.classList.add('squareG10');
+  }
+}
